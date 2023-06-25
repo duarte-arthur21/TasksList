c7352ed2b4ece40294736642c41754ab1924cb09
duarte-arthur21 <ferreiraduarte21@gmail.com>
2023-06-25 12:40:59 -0300

    Exclui pages obsoltas

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Home from "./src/pages/Home";
-import IMC from "./src/pages/IMC";
-import Conversor from "./src/pages/Conversor";
 import TodoList from "./src/pages/TodoList";
 import Login from "./src/pages/Login";
 import Register from "./src/pages/Register";
@@ -11,7 +9,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   Entypo,
-  MaterialIcons,
   MaterialCommunityIcons,
   FontAwesome5,
 } from "@expo/vector-icons";
@@ -39,26 +36,6 @@ function MyTabs() {
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="tasks" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Conversor"
-        component={Conversor}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="monetization-on" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="IMC"
-        component={IMC}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="arm-flex" color={color} size={size} />
           ),
         }}
       />
